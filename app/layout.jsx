9afc89f9573bafb7bbf66395c78c5dc6caad46df
com:crapsgame01 <crapsgame01@gmail.com>
2025-11-20 19:20:41 +0700

    Update layout.jsx
    Signed-off-by: com:crapsgame01 <crapsgame01@gmail.com>

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,34 @@
-"use client";
-
-import { useState } from "react";
+import NextNProgress from 'nextjs-progressbar';
+import Navbar from '@/components/Navbar';
+import './globals.css';
 
 export default function RootLayout({ children }) {
+  const neonColor = "#00e5ff"; 
+  
   return (
-    <html lang="th">
-      <body>{children}</body>
+    <html>
+      <head>
+        <title>GAME168 Console</title>
+      </head>
+      <body>
+        
+        <NextNProgress 
+          color={neonColor} 
+          startPosition={0.3} 
+          stopDelayMs={200} 
+          height={3} 
+          showOnShallow={true} 
+        />
+        
+        <Navbar />
+
+        {/* เพิ่ม padding-top เพื่อให้เนื้อหาไม่ถูก Navbar บัง */}
+        <div style={{ paddingTop: '5rem' }}> 
+          {children}
+        </div>
+        
+      </body>
     </html>
   );
 }
+
